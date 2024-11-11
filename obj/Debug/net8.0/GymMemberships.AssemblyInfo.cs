8491951ec1688328c88f703291ad4b42f62943fb
akmal015 <akmalarsyad015@gmail.com>
2024-11-12 00:24:17 +0800

    welcome

--- obj/Debug/net8.0/GymMemberships.AssemblyInfo.cs
+++ obj/Debug/net8.0/GymMemberships.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GymMemberships")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+345f9d132428b62f78b2411bc081d314acee23b1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fae1af879013c3e86bb36dc91b06a12c3c27df9a")]
 [assembly: System.Reflection.AssemblyProductAttribute("GymMemberships")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GymMemberships")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
